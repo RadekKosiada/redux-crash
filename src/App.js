f51b7f651844d77c0f7b71a7d278f11c72736ac5
RadekKosiada <kosiadar@gmail.com>
2019-05-23 17:40:34 +0200

    Wrap the App in Provider

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import './App.css';
+import { Provider } from 'react-redux';
+
 import Posts from './components/posts';
 import Postform from './components/postform';
+import { createStore, applyMiddleware } from 'redux';
+
+const store = createStore(() => [], {}, applyMiddleware());
 
 function App() {
   return (
-    <div className="App">
-      <Postform />
-      <hr />
-      <Posts />
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <Postform />
+        <hr />
+        <Posts />
+      </div>
+    </Provider>
   );
 }
 
